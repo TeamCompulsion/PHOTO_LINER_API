42e5dc7f6f0d0c4d3da6aa77a78b90f441be842b
강인화 <rkddlsghk1998@gmail.com>
2025-11-09 16:27:44 +0900

    init

---
+++ src/main/java/kr/kro/photoliner/PhotoLinerApplication.java
@@ -0,0 +1,13 @@
+package kr.kro.photoliner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoLinerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoLinerApplication.class, args);
+    }
+
+}
