3b69f9f9b9d693a46676b5f18ec82f0239213e05
chanrhan <km1104rs@naver.com>
2025-11-12 15:30:21 +0900

    chore(database): users, photos 테이블을 생성하는 V1__init.sql 마이그레이션 스크립트 작성

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,21 @@
+create table if not exists users
+(
+    id bigint unsigned auto_increment comment '사용자 고유 ID'
+        primary key,
+    username varchar(30) not null unique,
+    name varchar(30) not null ,
+    created_at datetime not null default current_timestamp,
+    updated_at datetime not null default current_timestamp
+);
+
+create table if not exists photos
+(
+    id bigint unsigned auto_increment comment '사진 고유 ID'
+        primary key,
+    file_name text not null,
+    file_path text not null,
+    captured_dt datetime not null,
+    location point not null,
+    user_id bigint not null,
+    created_at datetime not null default current_timestamp
+)
