15a31c5b5d598b26317157fd5edf9b2bce643d2e
chanrhan <km1104rs@naver.com>
2025-11-13 21:52:54 +0900

    refactor(photosResponse): snakecase strategy 제거

--- src/main/java/kr/kro/photoliner/domain/photo/dto/response/PhotosResponse.java
+++ src/main/java/kr/kro/photoliner/domain/photo/dto/response/PhotosResponse.java
@@ -1,18 +1,15 @@
 package kr.kro.photoliner.domain.photo.dto.response;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import kr.kro.photoliner.domain.photo.model.Photo;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@JsonNaming(value = SnakeCaseStrategy.class)
 public record PhotosResponse(
         Integer count,
         List<InnerPhotoResponse> photos
 ) {
-    public static PhotosResponse from(List<Photo> photos){
+    public static PhotosResponse from(List<Photo> photos) {
         return new PhotosResponse(
                 photos.size(),
                 photos.stream()
@@ -26,8 +23,8 @@ public record PhotosResponse(
             String filePath,
             LocalDateTime capturedDt,
             Long userId
-    ){
-        public static InnerPhotoResponse from(Photo photo){
+    ) {
+        public static InnerPhotoResponse from(Photo photo) {
             return new InnerPhotoResponse(
                     photo.getId(),
                     photo.getFilePath(),
