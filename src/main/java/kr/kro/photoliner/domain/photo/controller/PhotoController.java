373b7ab60e6c20bf25fe309e7691e3b30fbcd061
chanrhan <km1104rs@naver.com>
2025-11-14 17:17:30 +0900

    refactor(PhotoController): API 공통 버저닝 적용

--- src/main/java/kr/kro/photoliner/domain/photo/controller/PhotoController.java
+++ src/main/java/kr/kro/photoliner/domain/photo/controller/PhotoController.java
@@ -7,11 +7,13 @@ import kr.kro.photoliner.domain.photo.service.PhotoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class PhotoController {
 
     private final PhotoService photoService;
