99e2e5630e541cffd0554e33cf1ac2ad231fd23b
강인화 <rkddlsghk1998@gmail.com>
2025-11-16 23:28:21 +0900

    feat: 사진 수정 API 컨트롤러 추가

--- src/main/java/kr/kro/photoliner/domain/photo/controller/PhotoController.java
+++ src/main/java/kr/kro/photoliner/domain/photo/controller/PhotoController.java
@@ -3,21 +3,22 @@ package kr.kro.photoliner.domain.photo.controller;
 import jakarta.validation.Valid;
 import java.util.List;
 import kr.kro.photoliner.domain.photo.dto.request.MapMarkersRequest;
+import kr.kro.photoliner.domain.photo.dto.request.PhotoCapturedDateUpdateRequest;
+import kr.kro.photoliner.domain.photo.dto.request.PhotoLocationUpdateRequest;
 import kr.kro.photoliner.domain.photo.dto.response.MapMarkersResponse;
 import kr.kro.photoliner.domain.photo.dto.response.PhotoUploadResponse;
 import kr.kro.photoliner.domain.photo.dto.response.PhotosResponse;
-import kr.kro.photoliner.domain.photo.model.Photo;
 import kr.kro.photoliner.domain.photo.service.PhotoService;
 import kr.kro.photoliner.domain.photo.service.PhotoUploadService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -51,5 +52,23 @@ public class PhotoController {
     ) {
         PhotoUploadResponse response = photoUploadService.uploadPhotos(userId, files);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PatchMapping("/{photoId}/captured-date")
+    public ResponseEntity<Void> updatePhotoCapturedDate(
+            @PathVariable Long photoId,
+            @Valid @RequestBody PhotoCapturedDateUpdateRequest request
+    ) {
+        photoService.updatePhotoCapturedDate(photoId, request);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/{photoId}/location")
+    public ResponseEntity<Void> updatePhotoLocation(
+            @PathVariable Long photoId,
+            @Valid @RequestBody PhotoLocationUpdateRequest request
+    ) {
+        photoService.updatePhotoLocation(photoId, request);
+        return ResponseEntity.noContent().build();
     }
 }
