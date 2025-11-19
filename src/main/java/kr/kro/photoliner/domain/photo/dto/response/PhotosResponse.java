9b973f1d3bd8cc0ceda83533752945535bf11545
강인화 <164142264+kih1015@users.noreply.github.com>
2025-11-19 10:07:34 +0900

    fix: 널 처리 (#29)

--- src/main/java/kr/kro/photoliner/domain/photo/dto/response/PhotosResponse.java
+++ src/main/java/kr/kro/photoliner/domain/photo/dto/response/PhotosResponse.java
@@ -2,8 +2,10 @@ package kr.kro.photoliner.domain.photo.dto.response;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import kr.kro.photoliner.domain.photo.model.Photo;
 import kr.kro.photoliner.domain.photo.model.Photos;
+import org.locationtech.jts.geom.Point;
 
 public record PhotosResponse(
         Integer count,
@@ -23,18 +25,19 @@ public record PhotosResponse(
             Long id,
             String filePath,
             LocalDateTime capturedDt,
-            double lat,
-            double lng,
+            Double lat,
+            Double lng,
             Long userId
     ) {
 
         public static InnerPhotoResponse from(Photo photo) {
+            Optional<Point> location = Optional.ofNullable(photo.getLocation());
             return new InnerPhotoResponse(
                     photo.getId(),
                     photo.getFilePath(),
                     photo.getCapturedDt(),
-                    photo.getLocation().getY(),
-                    photo.getLocation().getX(),
+                    location.map(Point::getY).orElse(null),
+                    location.map(Point::getX).orElse(null),
                     photo.getUser().getId());
         }
     }
