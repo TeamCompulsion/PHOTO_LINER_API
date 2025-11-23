43cd6ef32a389d1efb8ca9c1800f3b78e4175b07
chanrhan <km1104rs@naver.com>
2025-11-23 10:41:00 +0900

    build: weblcient 의존성 추가

--- src/main/java/kr/kro/photoliner/domain/user/repository/UserRepository.java
+++ src/main/java/kr/kro/photoliner/domain/user/repository/UserRepository.java
@@ -5,6 +5,9 @@ import kr.kro.photoliner.domain.user.model.User;
 import org.springframework.data.repository.Repository;
 
 public interface UserRepository extends Repository<User, Long> {
+    boolean existsByEmail(String email);
 
+    User save(User user);
+    
     Optional<User> findUserById(Long userId);
 }
